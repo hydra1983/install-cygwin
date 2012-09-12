b6356dd8bda0ff388491df1d013d88121f044336
edison <hydra1983@gmail.com>
2012-09-12 18:13:53 +0800

    Fix env vars error for WIN7

--- install.bat
+++ install.bat
@@ -88,32 +88,48 @@ Rem -------------------------------------
   :env_xp
    
   Set SET_X=%CUR_DIR%/setx.exe
-  Set SET_X_M=-m
-  GoTo env_end
 
-  :env_win7
-  
-  Set SET_X=%windir%\system32\setx.exe /M
-  Set SET_X_M=
-  GoTo env_end
-
-  :env_end
-
-  Echo %SET_X% CYGWIN_HOME %CYGWIN_HOME%" %SET_X_M%
-  %SET_X% CYGWIN_HOME %CYGWIN_HOME% %SET_X_M%
+  Echo %SET_X% CYGWIN_HOME %CYGWIN_HOME%" -m
+  %SET_X% CYGWIN_HOME %CYGWIN_HOME% -m
 
   Echo %SET_X% HOME %USER_HOME%
   %SET_X% HOME %USER_HOME%
 
   SetLocal EnableDelayedExpansion
     If "%LAST_CHAR%"=="%SEMI_COMMA%" (
-      Echo %SET_X% PATH !PATH:%CYGWIN_HOME_BIN_PATH%=!%CYGWIN_HOME_BIN_PATH% %SET_X_M%
-      %SET_X% PATH "!PATH:%CYGWIN_HOME_BIN_PATH%=!%CYGWIN_HOME_BIN_PATH% %SET_X_M%"
+      Echo %SET_X% PATH !PATH:%CYGWIN_HOME_BIN_PATH%=!%CYGWIN_HOME_BIN_PATH% -m
+      %SET_X% PATH "!PATH:%CYGWIN_HOME_BIN_PATH%=!%CYGWIN_HOME_BIN_PATH% -m"
     ) Else (
-      Echo %SET_X% PATH !PATH:%CYGWIN_HOME_BIN_PATH%=!;%CYGWIN_HOME_BIN_PATH% %SET_X_M%
-      %SET_X% PATH "!PATH:%CYGWIN_HOME_BIN_PATH%=!;%CYGWIN_HOME_BIN_PATH% %SET_X_M%"
+      Echo %SET_X% PATH !PATH:%CYGWIN_HOME_BIN_PATH%=!;%CYGWIN_HOME_BIN_PATH% -m
+      %SET_X% PATH "!PATH:%CYGWIN_HOME_BIN_PATH%=!;%CYGWIN_HOME_BIN_PATH% -m"
     )   
   EndLocal
+
+  GoTo env_end
+
+  :env_win7
+  
+  Set SET_X=%windir%\system32\setx.exe
+
+  Echo %SET_X% /M CYGWIN_HOME %CYGWIN_HOME%"
+  %SET_X% /M CYGWIN_HOME %CYGWIN_HOME%
+
+  Echo %SET_X% HOME %USER_HOME%
+  %SET_X% HOME %USER_HOME%
+
+  SetLocal EnableDelayedExpansion
+    If "%LAST_CHAR%"=="%SEMI_COMMA%" (
+      Echo %SET_X% /M PATH !PATH:%CYGWIN_HOME_BIN_PATH%=!%CYGWIN_HOME_BIN_PATH%
+      %SET_X% /M PATH "!PATH:%CYGWIN_HOME_BIN_PATH%=!%CYGWIN_HOME_BIN_PATH%"
+    ) Else (
+      Echo %SET_X% /M PATH !PATH:%CYGWIN_HOME_BIN_PATH%=!;%CYGWIN_HOME_BIN_PATH%%
+      %SET_X% /M PATH "!PATH:%CYGWIN_HOME_BIN_PATH%=!;%CYGWIN_HOME_BIN_PATH%"
+    )   
+  EndLocal
+
+  GoTo env_end
+
+  :env_end
 
 Rem -------------------------------------
 Rem Custom cygwin
